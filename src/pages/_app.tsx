6a4e69d820baf354ecb6cfaeeb40b640a366af6e
Vinicius Benedito <beneditoloura@gmail.com>
2021-06-29 07:40:45 -0300

    feat: add createGlobalStyle, metas and favicons

---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link
+          rel="shortcut icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/img/icon-512.png"
+          type="image/x-icon"
+        />
+        <meta
+          name="description"
+          content="A simple proct starter to work with Typescript, React, NextJS and Styled Components"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
